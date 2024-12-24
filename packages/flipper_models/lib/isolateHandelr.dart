fd4f3550ea1a31abba63cb258f828bbccdf71ec8
Muragijimana <info@yegobox.com>
2024-12-24 12:13:12 +0200

    clean up the code

--- packages/flipper_models/lib/isolateHandelr.dart
+++ packages/flipper_models/lib/isolateHandelr.dart
@@ -36,13 +36,11 @@ mixin VariantPatch {
         iVariant.isrcAplcbYn =
             variant.isrcAplcbYn?.isEmpty ?? true ? "N" : variant.isrcAplcbYn;
 
-        /// do not attempt saving a variant with missing fields
         if (variant.qtyUnitCd == null ||
             variant.taxTyCd == null ||
             variant.bhfId == null ||
             variant.itemTyCd == null ||
             variant.bhfId!.isEmpty) {
-          // set defaults
           localRealm!.write(() {
             variant.taxTyCd = "B";
             variant.bhfId = "00";
@@ -73,7 +71,6 @@ mixin StockPatch {
     List<Stock> stocks =
         localRealm!.query<Stock>(r'ebmSynced ==$0', [false]).toList();
 
-    // // Fetching all variant ids from stocks
     List<int?> variantIds = stocks.map((stock) => stock.variantId).toList();
     Map<int, Variant?> variantMap = {};
     localRealm.query<Variant>(r'id IN $0', [variantIds]).forEach((variant) {
@@ -81,7 +78,6 @@ mixin StockPatch {
     });
     for (Stock stock in stocks) {
       if (!stock.ebmSynced) {
-        //     // Accessing variant from the pre-fetched map
         Variant? variant = variantMap[stock.variantId];
         if (variant == null) {
           continue;
@@ -98,11 +94,8 @@ mixin StockPatch {
           final response = await RWTax()
               .saveStockMaster(stock: iStock, variant: iVariant, URI: URI);
           if (response.resultCd == "000") {
-            // sendPort.send(
-            //     'notification:${response.resultMsg}:stock:${stock.id.toString()}');
             sendPort('Stock update:${response.resultMsg}');
           } else {
-            // sendPort.send('notification:${response.resultMsg}}');
             sendPort('Stock update:${response.resultMsg}}');
           }
         } catch (e) {
@@ -126,44 +119,25 @@ mixin PatchTransactionItem {
     print("transactions count ${transactions.length}");
     for (ITransaction transaction in transactions) {
       double taxB = 0;
-      // double taxC = 0;
-      // double taxA = 0;
-      // double taxD = 0;
+
       double totalvat = 0;
 
       Configurations taxConfigTaxB =
-          localRealm!.query<Configurations>(r'taxType == $0', ["B"]).first;
-      // Configurations taxConfigTaxA =
-      //     localRealm!.query<Configurations>(r'taxType == $0', ["A"]).first;
-      // Configurations taxConfigTaxC =
-      //     localRealm!.query<Configurations>(r'taxType == $0', ["C"]).first;
-      // Configurations taxConfigTaxD =
-      //     localRealm!.query<Configurations>(r'taxType == $0', ["D"]).first;
+          localRealm.query<Configurations>(r'taxType == $0', ["B"]).first;
 
-      List<TransactionItem> items = localRealm!.query<TransactionItem>(
+      List<TransactionItem> items = localRealm.query<TransactionItem>(
           r'transactionId == $0', [transaction.id]).toList();
 
       for (var item in items) {
         if (item.taxTyCd == "B") {
           taxB += (item.price * item.qty);
         }
-        // if (item.taxTyCd == "C") {
-        //   taxC += (item.price * item.qty);
-        // }
-        // if (item.taxTyCd == "A") {
-        //   taxA += (item.price * item.qty);
-        // }
-        // if (item.taxTyCd == "D") {
-        //   taxD += (item.price * item.qty);
-        // }
       }
-      // final totalTaxA = calculateTotalTax(taxA, taxConfigTaxA);
+
       final totalTaxB = calculateTotalTax(taxB, taxConfigTaxB);
-      // final totalTaxC = calculateTotalTax(taxC, taxConfigTaxC);
-      // final totalTaxD = calculateTotalTax(taxD, taxConfigTaxD);
+
       totalvat = totalTaxB;
 
-      /// stop if transaction does not have customerName or customerTin
       if (transaction.customerName == null || transaction.customerTin == null) {
         continue;
       }
@@ -175,7 +149,6 @@ mixin PatchTransactionItem {
             customerName: transaction.customerName ?? "N/A",
             custTin: transaction.customerTin ?? "999909695",
             regTyCd: "A",
-            //sarTyCd 11 is for sale
             sarTyCd: transaction.sarTyCd ?? "11",
             realm: localRealm,
             custBhfId: transaction.customerBhfId ?? "",
@@ -211,15 +184,13 @@ mixin CustomerPatch {
       required int tinNumber,
       required String bhfId,
       required int branchId}) async {
-    // load all customer
     List<Customer> customers =
         localRealm!.query<Customer>(r'branchId ==$0', [branchId]).toList();
 
     for (Customer customer in customers) {
       if (!customer.ebmSynced) {
         try {
-          localRealm!.write(() {
-            // Update customer properties within the write transaction
+          localRealm.write(() {
             customer.tin = tinNumber;
             customer.bhfId = bhfId;
           });
@@ -255,15 +226,13 @@ class IsolateHandler with StockPatch {
     final RootIsolateToken rootIsolateToken = args[1];
     DartPluginRegistrant.ensureInitialized();
     BackgroundIsolateBinaryMessenger.ensureInitialized(rootIsolateToken);
-    // Create a new ReceivePort for the isolate to receive messages
+
     ReceivePort port = ReceivePort();
-    // Send this ReceivePort's SendPort back to the main isolate for communication
-    /// for firestore it require to be re-initialized within isolate
+
     sendPort.send(port.sendPort);
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
-    // final firestore = FirebaseFirestore.instance;
 
     port.listen((message) async {
       if (message is Map<String, dynamic>) {
@@ -276,30 +245,7 @@ class IsolateHandler with StockPatch {
           localRealm = Realm(configLocal);
         }
 
-        if (message['task'] == 'sync') {
-          // int businessId = message['businessId'];
-          // int branchId = message['branchId'];
-          // int userId = message['userId'];
-
-          /// query products from firestore
-          // firestore.collection(stocksTable).limit(1).get().then((value) {
-          //   for (var doc in value.docs) {
-          //     // final stock = doc.data();
-          //     sendPort.send("notification:${doc.data()['stock_id']}");
-          //     // final stock = Stock.fromJson(stock);
-          //   }
-          // });
-
-          /// un comment this when https://github.com/flutter/flutter/issues/119207
-          // await clearFirestoreCache();
-          // await PullChange().startAsync(
-          //   mbusinessId: businessId,
-          //   firestore: firestore,
-          //   localRealm: localRealm!,
-          //   mbranchId: branchId,
-          //   muserId: userId,
-          // );
-        }
+        if (message['task'] == 'sync') {}
         if (message['task'] == 'taxService') {
           int branchId = message['branchId'];
           int tinNumber = message['tinNumber'];
@@ -308,8 +254,6 @@ class IsolateHandler with StockPatch {
           String bhfId = message['bhfId'];
           String encryptionKey = message['encryptionKey'];
           String local = message['dbPath'];
-          //  sendPort.send('notification:hello');
-          // BackgroundIsolateBinaryMessenger.ensureInitialized(rootIsolateToken);
 
           LocalConfiguration configLocal =
               localConfig(encryptionKey.toIntList(), local);
@@ -318,10 +262,8 @@ class IsolateHandler with StockPatch {
             localRealm = Realm(configLocal);
           }
 
-          /// handle missing value, part of self healing
           _selfHeal(localRealm: localRealm);
 
-          //  patch transactionItem
           PatchTransactionItem.patchTransactionItem(
             URI: URI,
             sendPort: (message) {
@@ -372,11 +314,9 @@ class IsolateHandler with StockPatch {
       schemaVersion: schemaVersion,
       migrationCallback: (migration, oldSchemaVersion) {
         if (oldSchemaVersion < 2) {
-          // This means we are migrating from version 1 to version 2
           migration.deleteType('Drawers');
         }
         if (oldSchemaVersion < 7) {
-          // This means we are migrating from version 1 to version 2
           migration.deleteType('Log');
         }
       },
@@ -386,7 +326,7 @@ class IsolateHandler with StockPatch {
 
   static Future<void> localData(List<dynamic> args) async {
     final rootIsolateToken = args[0] as RootIsolateToken;
-    // final sendPort = args[1] as SendPort;
+
     final dbPatch = args[3] as String;
     String? key = args[4] as String?;
 
@@ -406,26 +346,10 @@ class IsolateHandler with StockPatch {
     DartPluginRegistrant.ensureInitialized();
 
     localRealm = Realm(config);
-    // List<UnversalProduct> codes = localRealm!
-    //     .query<UnversalProduct>(r'branchId==$0', [branchId]).toList();
-    // if (codes.isEmpty) {
-    //   final Completer<void> completer = Completer<void>();
-    //   Timer(Duration(seconds: 5), () {
-    //     if (!completer.isCompleted) {
-    //       sendPort.send(1);
-    //     }
-    //   });
-    //   print("Empty code fetching now");
-    //   // fetchDataAndSaveUniversalProducts(businessId, branchId, URI, bhfid);
-    //   sendPort.send(1);
-    //   completer.complete();
-    // }
   }
 
-  // Function to fetch data from the URL endpoint
   static Future<void> fetchDataAndSaveUniversalProducts(
       int businessId, int branchId, String URI, String bhfid) async {
-    // final talker = TalkerFlutter.init();
     try {
       Business business =
           localRealm!.query<Business>(r'serverId == $0', [businessId]).first;
@@ -435,29 +359,23 @@ class IsolateHandler with StockPatch {
       final body = jsonEncode({
         "tin": business.tinNumber,
         "bhfId": bhfid,
-
-        ///TODO: change this date to a working date in production
         "lastReqDt": "20190523000000",
       });
       print("Loading item codes");
       final response =
           await http.post(Uri.parse(url), headers: headers, body: body);
       if (response.statusCode == 200) {
-        // Parse the JSON response
         final jsonResponse = json.decode(response.body);
 
-        // Check if the response contains the data and itemClsList
         if (jsonResponse['data'] != null &&
             jsonResponse['data']['itemClsList'] != null) {
           final List<dynamic> itemClsList = jsonResponse['data']['itemClsList'];
 
-          // Loop through the itemClsList and print the itemClsNm (name)
           for (var item in itemClsList) {
             final UniversalProduct product = UniversalProduct.fromJson(item);
             UnversalProduct? uni = localRealm!.query<UnversalProduct>(
                 r'itemClsCd == $0', [product.itemClsCd]).firstOrNull;
             if (uni == null) {
-              // talker.info("Now saving universal");
               localRealm!.write(() {
                 localRealm!.add(
                   UnversalProduct(
@@ -482,33 +400,22 @@ class IsolateHandler with StockPatch {
       } else {
         print('Failed to load data. Status code: ${response.statusCode}');
       }
-    } catch (e) {
-      // print('Error fetching data: $e');
-    }
+    } catch (e) {}
   }
 
-  /// handle properties added later as the app grow but needed to support old clients
   static void _selfHeal({Realm? localRealm}) {
-    /// query all variants
-
-    // Query stocks where sold == 0.0
     List<Stock> stocks = localRealm!.query<Stock>(r'variant == NULL').toList();
 
-    // Loop through each stock to calculate and update sold quantity
     for (Stock stock in stocks) {
-      // Query past transaction items for the stock's variant
       Variant? variant =
           localRealm.query<Variant>(r'id == $0', [stock.variantId]).firstOrNull;
 
-      // Write updated sold quantity to realm
       localRealm.write(() {
         if (variant != null) {
           print("Healed Stock: ${stock.id}");
         }
       });
     }
-
-    /// query stock with variant null assign it
 
     List<Variant> vVariants =
         localRealm.query<Variant>(r'productName == NULL').toList();
@@ -523,7 +430,6 @@ class IsolateHandler with StockPatch {
       }
     }
 
-    /// first find any variant with empty itemClsCd add defaults
     List<Variant> variants = localRealm
         .query<Variant>(r'itemClsCd == null OR itemClsCd == ""')
         .toList();
@@ -542,8 +448,6 @@ class IsolateHandler with StockPatch {
         item.itemClsCd = "5020230602";
       });
     }
-
-    /// track variant without stock and match them with stock
 
     List<Variant> variantsall = localRealm.all<Variant>().toList();
 
@@ -572,7 +476,6 @@ class IsolateHandler with StockPatch {
       }
     }
 
-    /// Loop through Configuration and remove any duplicate configuration found e.g there should be one Conciguration.taxType
     List<Configurations> configurations =
         localRealm.all<Configurations>().toList();
     Set<String> uniqueTaxTypes = {};
@@ -604,7 +507,6 @@ class IsolateHandler with StockPatch {
     List<Variant> variantsAll = localRealm.all<Variant>().toList();
     List<ITransaction> transactions = localRealm.all<ITransaction>().toList();
     for (ITransaction transaction in transactions) {
-      // find equivalent transactionRecord
       final TransactionPaymentRecord? record = localRealm
           .query<TransactionPaymentRecord>(
               r'transactionId == $0', [transaction.id]).firstOrNull;
@@ -624,7 +526,6 @@ class IsolateHandler with StockPatch {
         localRealm.query<Stock>(r'initialStock == NULL').toList();
 
     for (Stock stock in stockss) {
-      // find equivalent transactionRecord
       if (stock.initialStock == null) {
         localRealm.write(() {
           stock.initialStock = stock.currentStock;
@@ -632,10 +533,8 @@ class IsolateHandler with StockPatch {
       }
     }
 
-    /// check for variant that do not have stock assigned asign it
     for (Variant variant in variantsAll) {
       if (variant.stock == null) {
-        /// find stock assign it
         Stock? stock = localRealm.query<Stock>(
             r'variantId ==$0 && branchId == $1',
             [variant.id, variant.branchId]).firstOrNull;
@@ -662,7 +561,7 @@ class IsolateHandler with StockPatch {
                 active: false,
               ),
             );
-            // get created stock
+
             final stock =
                 localRealm.query<Stock>(r'id == $0', [id]).firstOrNull;
             variant.stock = stock;
